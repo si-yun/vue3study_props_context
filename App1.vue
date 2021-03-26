d54db11f11009ea83e4e23b942f9a92617d28bf3
Siyun <syun.m2@kakao.com>
2021-03-26 18:13:03 +0900

    Add files via upload

---
+++ App1.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HelloWorld message="gg" @click="consoleLog" class="kk">
+      <div>slots</div>
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const consoleLog = (value) => console.log("click!" + value);
+    return { consoleLog };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ update }}
+  </div>
+  <slot></slot>
+  <button @click="passData">CLICK</button>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  props: {
+    message: {
+      type: String,
+      require: true,
+    },
+  },
+  //context {emit,attrs,slots}
+  setup(props, { emit, attrs, slots }) {
+    //setup 안에서는 this가 안됨
+    console.log(attrs.class);
+    console.log(slots.default());
+    const update = computed(() => props.message + "!");
+    const passData = () => emit("click", "hi!");
+    return {
+      update,
+      passData,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
